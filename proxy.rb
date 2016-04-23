14922117508b00b719801ea2cf2f9ba8204d7c4e
Matthew Landauer <mlandauer@gmail.com>
2016-04-24 07:44:32 +1000

    Render another page with a haml view

--- proxy.rb
+++ proxy.rb
@@ -3,3 +3,10 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+# An example dynamic page that can be used to demonstrate the
+# proxy. This page is a simple page that gets loaded via javascript
+# (in this case React).
+get '/example_dynamic_page' do
+  haml :example_dynamic_page
+end
