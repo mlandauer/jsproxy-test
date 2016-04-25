c45d96d924edaff6df02e1525a71ec71cad7fd21
Matthew Landauer <mlandauer@gmail.com>
2016-04-26 08:41:46 +1000

    Make rendering little more solid by waiting for half a second

--- phantomjs/get.js
+++ phantomjs/get.js
@@ -6,18 +6,25 @@
 var page = require('webpage').create();
 var url = "http://localhost:4567/example_dynamic_page";
 
+function doRender(page) {
+  var html = page.evaluate(function() {
+    // Magically this bit will get evaluated in the context of the page
+    return document.documentElement.innerHTML;
+  });
+  console.log(html);
+
+  phantom.exit();
+}
+
 // This displays console messages from inside the page.evaluate block
 page.onConsoleMessage = function(msg) {
   console.log('console:', msg);
 };
 
 page.open(url, function (status) {
-  var html = page.evaluate(function() {
-    // Magically this bit will get evaluated in the context of the page
-    return document.documentElement.innerHTML;
-  });
-
-  console.log(html);
-
-  phantom.exit();
+  // As a first (very rough) pass wait for a
+  // half second before rendering the page
+  setTimeout(function() {
+    doRender(page);
+  }, 500);
 });
